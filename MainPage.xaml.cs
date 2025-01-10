bb6acad109634a79534bdf161f9e298c9cac72a1
druchti1 <79176482+druchti1@users.noreply.github.com>
2025-01-10 11:23:30 -0600

    added gitignore

---
+++ MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace TestApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Card1Clicked(object sender, EventArgs e)
+        {
+            Card1.IsVisible = false;
+            Card1.IsEnabled = false;
+            Card2.IsVisible = true;
+            Card2.IsEnabled = true;
+            Card2.Source = "c10c.png";
+        }
+
+        private void Card2Clicked(object sender, EventArgs e)
+        {
+            Card2.IsVisible = false;
+            Card2.IsEnabled = false;
+            Card1.IsVisible = true;
+            Card1.IsEnabled = true;
+            Card1.Source = "c10c.png";
+        }
+    }
+}
